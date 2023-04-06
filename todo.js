5e84a667776a455451853c7b2204d28e5dec53f2
Tilde <joti22eo@student.ju.se>
2023-04-06 20:53:42 +0200

    Added delete button and mark task as done

--- todo.js
+++ todo.js
@@ -5,10 +5,55 @@ function onLoadHandler() {
 
 // Add new task to the tasklist
 function addToTaskList() {
-  const inputElement = document.getElementById("input").value;
-  const addedTask = document.createElement("li");
-  addedTask.innerText = inputElement;
-  document.getElementById("taskList").appendChild(addedTask);
+  const inputElement = document.getElementById("input");
+  const inputValue = inputElement.value;
+
+  if (inputValue.trim() === "") {
+    // Check if input value is empty or contains only whitespace
+    alert("Please enter a task.");
+    return;
+  }
+
+  const taskListElement = document.getElementById("taskList");
+  const addedTaskElement = document.createElement("li");
+  addedTaskElement.innerText = inputValue;
+
+  // Delete an added task
+  // Create a delete button
+  const deleteButtonElement = document.createElement("button");
+  deleteButtonElement.innerText = "Delete";
+
+  // Style for delete button
+  deleteButtonElement.style.backgroundColor = "#eee9e6";
+  deleteButtonElement.style.border = "none";
+  deleteButtonElement.style.padding = "5px 10px";
+  deleteButtonElement.style.cursor = "pointer";
+  deleteButtonElement.style.borderRadius = "40px";
+  deleteButtonElement.style.marginLeft = "20px";
+
+  // Delete task with the delete button
+  deleteButtonElement.addEventListener("click", function () {
+    addedTaskElement.remove();
+  });
+
+  addedTaskElement.innerText = inputValue;
+
+  // Mark task as done or not
+  addedTaskElement.addEventListener("click", function () {
+    if (addedTaskElement.style.textDecoration === "line-through") {
+      addedTaskElement.style.textDecoration = "none";
+      addedTaskElement.style.color = "#82674e";
+    } else {
+      addedTaskElement.style.textDecoration = "line-through";
+      addedTaskElement.style.color = "gray";
+    }
+  });
+
+  addedTaskElement.appendChild(deleteButtonElement);
+  taskListElement.appendChild(addedTaskElement);
+
+  // Clear input value after adding task
+  inputElement.value = "";
 }
 
 window.addEventListener("load", onLoadHandler);

3265c9f9c6b57ddc914325f737725f47e7f5407d
Tilde <joti22eo@student.ju.se>
2023-04-07 19:39:12 +0200

    Cleaned up the code
    Made a backup doc for experimenting with the code

--- backup.js
+++ backup.js
@@ -1,7 +1,6 @@
 function onLoadHandler() {
-  // Clicking the "+" button -> addToTaskList function
-  const addButtonElement = document.getElementById("addButton");
-  addButtonElement.addEventListener("click", addToTaskList);
+  const addNewTaskElement = document.getElementById("addButton");
+  addNewTaskElement.addEventListener("click", addToTaskList);
 }
 
 // Add new task to the tasklist
@@ -9,32 +8,29 @@ function addToTaskList() {
   const inputElement = document.getElementById("input");
   const inputValue = inputElement.value;
 
-  // Check if input value is empty or contains only whitespace
   if (inputValue.trim() === "") {
+    // Check if input value is empty or contains only whitespace
     alert("No task has been entered");
     return;
   }
 
-  // Makes a list out of the input value
   const taskListElement = document.getElementById("taskList");
   const addedTaskElement = document.createElement("li");
   addedTaskElement.innerText = inputValue;
 
+  // Delete an added task
   // Create a delete button
   const deleteButtonElement = document.createElement("button");
   deleteButtonElement.innerText = "Delete";
   deleteButtonElement.classList.add("deleteButton");
 
   // Delete task with the delete button
-  deleteButtonElement.addEventListener("click", removeElement);
-  function removeElement() {
-    const element = this.parentNode;
-    element.parentNode.removeChild(element);
-  }
+  deleteButtonElement.addEventListener("click", function () {
+    addedTaskElement.remove();
+  });
 
   // Mark task as done or not
-  addedTaskElement.addEventListener("click", markTask);
-  function markTask() {
+  addedTaskElement.addEventListener("click", function () {
     if (addedTaskElement.style.textDecoration === "line-through") {
       addedTaskElement.style.textDecoration = "none";
       addedTaskElement.style.color = "#82674e";
@@ -42,11 +38,9 @@ function addToTaskList() {
       addedTaskElement.style.textDecoration = "line-through";
       addedTaskElement.style.color = "gray";
     }
-  }
+  });
 
-  // Makes delete button a child of addedTaskElement
   addedTaskElement.appendChild(deleteButtonElement);
-  // Makes added task a child of taskLiskElement
   taskListElement.appendChild(addedTaskElement);
 
   // Clear input value after adding task
